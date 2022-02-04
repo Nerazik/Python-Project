85e89a0081060b26bd4e8d27ef36e105f39d6ee9
Redl <nerazik2018@mail.ru>
2022-02-04 22:01:30 +0300

    GG_2

---
+++ lab2_3.py
@@ -0,0 +1,16 @@
+x = 18
+y = 6
+a = 27
+d = (x+a*y)**5+1
+z = x-y-a**3
+abs(z)
+c = a-z
+v = d/c
+q = a**2+x**3
+w = y**3-a
+e = q/w
+f = v+e
+print(f)
+print(round(f))
+print(round(f, 1))
+print(round(f, 2))
---
+++ lab4_3.py
@@ -0,0 +1,13 @@
+n = int(input("Введите цифру от 0 до 4\n"))
+if n == 0:
+    print('Ноль')
+elif n == 1:
+    print('Один')
+elif n == 2:
+    print('Два')
+elif n == 3:
+    print(' Три')
+elif n == 4:
+    print('Четыре')
+else:
+    print("Неверно введены данные!")

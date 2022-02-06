be7895a63b25aa1e68d7fefe81aa6c2674c90f9d
Redl <nerazik2018@mail.ru>
2022-02-06 10:09:38 +0300

    GG_4

--- math.py
+++ math.py
@@ -1,14 +1,14 @@
 from math import *
-def ctg(x):
-    return cos(x) / sin(x)
+def ctg(k):
+    return cos(k) / sin(k)
 a = float(input("Введите значения параметра a:\n"))
 k = -1
 while True:
     if round(k * 2 - a, 2) == 0:
      k += 0.3
     continue
-    z = (math.log(fabs(ctg(a**2))- k*1)) / (k*2) 
-    print("{0} {1:.2f} {2} {3:.4f}" .format("x =", x, "z(a) = ", z))
+    z = (math.log(fabs(ctg(a**2))- k*1)) / (k*2) - a
+    print("{0} {1:.2f} {2} {3:.4f}" .format("k =", k, "z(a) = ", z))
     k += 0.3
-    if x > 6:
+    if k > 6:
      break
